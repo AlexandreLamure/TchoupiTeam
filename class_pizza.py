72973b54a353c5431b981a848e0aef7d4caf8d81
DESKTOP-29KQ7L4\César <ce.belley@gmail.com>
2018-02-28 22:07:27 +0100

    class pizza

---
+++ class_pizza.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 28 21:48:20 2018
+
+@author: César
+"""
+
+class Part:
+    
+    def __init__(self,x0,y0,x1,y1):
+        self.x0 = x0
+        self.y0 = y0
+        self.x1 = x1
+        self.y2 = y1
+        
+
+class Pizza:
+    
+    def __init__(self):
+        self.listPart = []
+        
+    def slicep(self, pizza,x,y,x1,y1):
+        if x1 >= len(pizza) or y >= len(pizza[0]):
+            return False
+        if x < 0 or y < 0:
+            return False
+        self.listPart.append(Part.part(x,y,x1,y1))
+        return True
+    
