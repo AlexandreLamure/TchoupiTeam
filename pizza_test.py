eeea6e7d17c4c7cbfe30478a3b78b6cdfb9982a6
Alexandre Lamure <alexandre.lamure@epita.fr>
2018-02-28 22:37:05 +0100

    ADD pizza_test.py to launch tests

---
+++ pizza_test.py
@@ -0,0 +1,13 @@
+from class_pizza import *
+from dumb_1 import *
+from pizza_generator import *
+
+import sys
+
+def launch_test():
+  if len(sys.argv) != 2:
+    sys.exit()
+  n = int(sys.argv[1])
+  for i in range(0, n):
+    r, c, l, h, tab = new_pizza()
+    data = dumb_1(r, c, l , h,)
