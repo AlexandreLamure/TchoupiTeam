85adcf2e00ade217bdb1c2ac52a310b0a2522467
Maxime L'Hermite <maxime.lhermite@epita.fr>
2018-02-28 21:53:31 +0100

    dumb 1 slice(array pizza, x coord, y coord, width, height)

--- pizza.py
+++ pizza.py
@@ -1,3 +1,8 @@
 from pizza import *
 
 width, height, minimum, area, pizza = start()
+
+def dumb_1():
+    for y in range(0, len(pizza)):
+        for x in range(0, len(pizza[0])):
+            slice(pizza, x, y, area, 1)
