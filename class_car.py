3af4446986ddf7a0412d72d004d166291b207e10
DESKTOP-29KQ7L4\César <ce.belley@gmail.com>
2018-03-01 19:06:44 +0100

    car

---
+++ class_car.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  1 19:04:25 2018
+
+@author: César
+"""
+
+class car:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.myCourse = []
