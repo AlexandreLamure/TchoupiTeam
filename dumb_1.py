5a8a6265b11458e0dd4b31ce6cd9bf44f8569bcb
Maxime L'Hermite <maxime.lhermite@epita.fr>
2018-03-01 18:11:44 +0100

    reset

--- dumb_1.py
+++
@@ -1,8 +0,0 @@
-from class_pizza import *
-
-def dumb_1(width, height, minimum, area, pizza):
-    data = Pizza(width, height, minimum, 1, pizza)
-    for y in range(0, len(pizza)):
-        for x in range(0, len(pizza[0])):
-            data.slicep(pizza, x, y, area, 1)
-    data.save()
--- pizza.py
+++
@@ -1,8 +0,0 @@
-from pizza import *
-from dumb_1 import *
-from parsing import *
-
-width, height, minimum, area, pizza = start()
-
-
-dumb_1(width, height, minimum, area, pizza)
