2c58f610b8e6fff48a969a417975f43dbcbe4aa8
Alexandre Lamure <alexandre.lamure@epita.fr>
2018-03-01 19:34:16 +0100

    main loop

--- calss_ride.py
+++ calss_ride.py
@@ -8,11 +8,13 @@ Created on Thu Mar  1 19:12:48 2018
 from math import sqrt
 
 class Ride:
-    def __init__(self, x0, y0, x1, y1):
+    def __init__(self, x0, y0, x1, y1, e, l):
         self.x0_ = x0
         self.y0_ = y0
         self.x1_ = x1
         self.y1_ = y1
+        self.early = e
+        self.last = l
         self.distance = self.distancePoints(x0, y0, x1, y1)
 
     
--- hash.py
+++ hash.py
@@ -9,11 +9,31 @@ if not os.path.exists("results"):
     os.makedirs("results")
 fichier = open("results/result_"+ str(time.time())+ "_"+str(0)+".txt","w")
 
+
+for i in range(0, n):
+    x0, y0, x1, y1, e, l = courses[i]
+    rides.append(ride(x0, y0, x1, y1, e, l))
+
+for i in range(0, f):
+    cars.append(car())
+
+# Main Loop
+for i in range(0, t):
+    dispatch()
+    for car_tmp in cars:
+        #move
+
+'''
 resp= str(n)
 for i in range(n/f + 1):
     resp += ' ' + str(i)
 
 for i in range(f - 1):
     resp += '\n0'
+'''
+
+for i in range(0, f):
+    cars[i].rideToString()
+
 fichier.write(resp)
 fichier.close()
