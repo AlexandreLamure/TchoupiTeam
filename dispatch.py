9f65c3101712d303db84c5779f3583eedd3ee613
Sangbin Lee <sangbin.lee@epita.fr>
2018-03-01 21:18:07 +0100

    dumb dispatch

--- dispatch.py
+++ dispatch.py
@@ -1,12 +1,31 @@
 from class_ride import *
 from class_car import *
 
-def dispatch(cars, rides):
-    for c in cars:
-        if (c.free_):
-            for i in range(0, len(rides)):
-                if not rides[i].finished:
-                    c.free_ = False
-                    c.ride_ = rides[i]
-                    break
+
+def dst(x0, y0, x1, y1):
+        return  abs(y1 - y0) + abs(x1 - x0)
+
+
+def best_ride(car, rides):
+    res = 0
+    g = 
+    l = 0
+    for i in range(0, len(rides)):
+        if rides[i].finished:
+            continue
+        dist = rides[i].distanceToStart(car)
+        dist2 = rides[i].early - car.step
+
+        if dist2 - dist == 0:
+            return i
+
+        elif dist2 - dist < 0:
+            if 
+            if rides[i].early < car.step:
+                if rides[i].early < rides[g].early:
+                    g = i
+
+
             
+
+
