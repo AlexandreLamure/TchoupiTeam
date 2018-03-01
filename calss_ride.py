84f2e03040e9b78fa2ed2a0378d4f810f521f626
DESKTOP-29KQ7L4\César <ce.belley@gmail.com>
2018-03-01 19:21:08 +0100

    class

--- calss_ride.py
+++ calss_ride.py
@@ -5,10 +5,12 @@ Created on Thu Mar  1 19:12:48 2018
 @author: César
 """
 
+from math import sqrt
+
 class Ride:
     def __init__(self, x0, y0, x1, y1):
         self.x0_ = x0
         self.y0_ = y0
         self.x1_ = x1
         self.y1_ = y1
-        self.distance = (y1 - y0) + (x1 - x0)
+        self.distance = sqrt((y1 - y0)**2 + (x1 - x0)**2)
--- class_car.py
+++ class_car.py
@@ -5,6 +5,8 @@ Created on Thu Mar  1 19:04:25 2018
 @author: César
 """
 
+
+
 class Car:
     def __init__(self,x,y):
         self.x_ = x
